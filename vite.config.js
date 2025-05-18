30aa9092597461d2da4a4ba22337b07f6d3e6303
Xiaomei <1812165896@qq.com>
2025-05-19 00:38:33 +0800

    修改code组件代码高亮

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? '/mei-portfolio/' : '/'
+  // Vercel 部署需要使用根路径
+  base: '/'
+  // 如果您需要为不同平台使用不同配置，可以使用环境变量
+  // base: process.env.DEPLOY_TARGET === 'github' ? '/mei-portfolio/' : '/'
 })
